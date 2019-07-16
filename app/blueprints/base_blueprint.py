f814e610119d6c49b47a3274dca9d757bd0ddf85
odur <odurjoseph8@gmail.com>
2019-07-16 11:18:06 +0300

    ft(category): Add category blueprint to base prints
     - Add category blueprint to base prints
    
    [Maintains #165367009]

--- app/blueprints/base_blueprint.py
+++ app/blueprints/base_blueprint.py
@@ -15,10 +15,11 @@ class BaseBlueprint:
 		''' Register All App Blue Prints Here '''
 		
 		from app.blueprints import (
-			home_blueprint, attribute_blueprint, customer_blueprint, department_blueprint
+			home_blueprint, attribute_blueprint, customer_blueprint, department_blueprint, category_blueprint
 		)
 
 		self.app.register_blueprint(attribute_blueprint)
 		self.app.register_blueprint(home_blueprint)
 		self.app.register_blueprint(customer_blueprint)
 		self.app.register_blueprint(department_blueprint)
+		self.app.register_blueprint(category_blueprint)
